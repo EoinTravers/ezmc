3ed7512761248bb98ba5d0ebfde093baef109cd0
Eoin Travers <eoint02@gmail.com>
2019-08-14 15:28:07 +0100

    More readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -9,20 +9,27 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
 
-project = 'EZMC'
+project = 'ezmc'
 copyright = '2019, Eoin Travers'
 author = 'Eoin Travers'
-version = '0.0.1'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
 
 
-# -- General configuration -
-# These might not all still be needed.
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -32,13 +39,14 @@ extensions = [
     'sphinx.ext.viewcode'
 ]
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+                    '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,10 +54,9 @@ templates_path = ['_templates']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
-# html_theme = "flask"
 html_theme = "classic"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
